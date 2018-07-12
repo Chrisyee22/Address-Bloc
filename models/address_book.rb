1a7cd69386818f901738a5743f0f44b4994e6195
Chris <chris.anthony.yee@gmail.com>
2018-07-12 16:06:07 -0700

    updated for ORM use v1

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,82 +3,45 @@ require "csv"
 require 'bloc_record/base'
 
 class AddressBook < BlocRecord::Base
-    attr_reader :entries
+  attr_reader :entries
 
-    def initialize(options={})
-        super
-        @entries = []
+  def initialize(options={})
+     super
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+     Entry.create(name: name, phone_number: phone, email: email, address_book_id: self.id)
+  end
+
+  def import_from_csv(file_name)
+    # Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
     end
 
-    def nuke
-        @entries = []
-    end
-
-    def remove_entry(name, phone_number, email)
-       delete_entry = nil
-
-       @entries.each do |entry|
-           if name == entry.name && phone_number == entry.phone_number && email == entry.email
-               delete_entry = entry
-           end
-        end
-
-       @entries.delete(delete_entry)
-    end
-
-    def add_entry(name, phone_number, email)
-
-        index = 0
-        entries.each do |entry|
-
-            if name < entry.name
-                break
-            end
-                index+= 1
-            end
-
-            entries.insert(index, Entry.new(name, phone_number, email))
-        end
-
-        def import_from_csv(file_name)
-
-          csv_text = File.read(file_name)
-          csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-
-          csv.each do |row|
-              row_hash = row.to_hash
-              add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-          end
-        end
-
-        def iterative_search(name)
-            @entries.each do |entry|
-                if entry.name == name
-                  return entry
-                end
-           end
-            return nil
-        end
-
-        def binary_search(name)
-
-            lower = 0
-            upper = entries.length - 1
-
-            while lower <= upper
-                mid =(lower + upper) / 2
-                mid_name = entries[mid].name
-
-                if name == mid_name
-                    return entries[mid]
-                elsif name < mid_name
-                    upper = mid - 1
-                elsif name > mid_name
-                    lower = mid + 1
-                end
-            end
-
-            return nil
-        end
-
-    end
+    return nil
+  end
+end
